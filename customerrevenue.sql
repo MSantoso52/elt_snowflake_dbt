a1cc79917afb8b0d85078cbb5380697da99118c3
MSantoso52 <santosomulyo52@gmail.com>
2025-08-20 20:34:55 +0700

    Update models & lineage

--- customerrevenue.sql
+++ customerrevenue.sql
@@ -1,11 +1,21 @@
-select
-  os.customer_id,
-  c.full_name,
-  sum(os.ordercount) as ordercount,
-  sum(os.revenue) as revenue
-from 
-  {{ ref('orders_fact') }} os
-join {{ ref('customers_stg') }} c on c.customer_id = os.customer_id
-group by
-  os.customer_id,
-  c.full_name
+{{ config(materialized='table') }}
+with customerrevenue as(
+  select
+    c.customer_id,
+    c.full_name as customername,
+    count(distinct o.order_id) as ordercount,
+    sum(oi.item_quantity * oi.item_unit_price) as revenue
+  from customers c
+  join orders o on c.customer_id = o.customer_id
+  join orderitems oi on o.order_id = oi.order_id
+  group by
+    c.customer_id,
+    c.full_name
+  order by
+    ordercount,
+    revenue
+    desc
+)
+
+select customer_id, customername, ordercount, revenue
+from customerrevenue
